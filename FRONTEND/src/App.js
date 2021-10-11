72426ebf56b6effeade17a87102c7e9688aa706a
spandan1460 <1929250@kiit.ac.in>
2021-10-11 18:30:48 +0530

    The final page with features section

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import About from './components/About/About';
-import Features from './components/Landing/Landing';
+import Features from './components/Features/Features';
 import Landing from './components/Landing/Landing';
 
 function App() {
