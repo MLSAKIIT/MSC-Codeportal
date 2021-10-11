b3d49bfb1d32d54a76080501ab61a0601ad75bca
spandan1460 <1929250@kiit.ac.in>
2021-10-11 13:56:42 +0530

    Added a height between two sections

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import About from './components/About/About';
+import Features from './components/Landing/Landing';
 import Landing from './components/Landing/Landing';
 
 function App() {
