d4150772f44f21c22928555d562b3329116bbadf
Medhavi Basera <1905781@kiit.ac.in>
2021-10-18 22:47:33 +0530

    Dashboard folder added

--- FRONTEND/FRONTEND/src/components/ContentBlock/index.tsx
+++
@@ -1,9 +0,0 @@
-import RightContentBlock from "./RightContentBlock";
-import { ContentBlockProps } from "./types";
-
-const ContentBlock = (props: ContentBlockProps) => {
-  if (props.type === "right") return <RightContentBlock {...props} />;
-  return null;
-};
-
-export default ContentBlock;
