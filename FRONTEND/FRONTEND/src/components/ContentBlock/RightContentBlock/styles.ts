549c966d2fb19d2c9ecaa232202c025a4496d715
Medhavi Basera <1905781@kiit.ac.in>
2021-10-18 22:05:52 +0530

    Initial changes

---
+++ FRONTEND/FRONTEND/src/components/ContentBlock/RightContentBlock/styles.ts
@@ -0,0 +1,41 @@
+import styled from "styled-components";
+
+export const RightBlockContainer = styled("section")`
+  position: relative;
+  // this is the place that controls the padding of the header content
+  padding: 5.2rem 0 8rem;
+
+  @media only screen and (max-width: 1024px) {
+    padding: 8rem 0 6rem;
+  }
+
+  @media only screen and (max-width: 768px) {
+    padding: 4rem 0 3rem;
+  }
+`;
+
+export const Content = styled("p")`
+  margin: 1.5rem 0 2rem 0;
+  color : white;
+  font-weight: 600;
+`;
+
+export const ContentWrapper = styled("div")`
+  position: relative;
+  max-width: 540px;
+
+  @media only screen and (max-width: 575px) {
+    padding-bottom: 4rem;
+  }
+`;
+
+export const ButtonWrapper = styled("div")`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+
+  @media screen and (min-width: 1024px) {
+    max-width: 80%;
+  }
+
+`;
