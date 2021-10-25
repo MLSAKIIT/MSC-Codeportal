bee3a3b90fc459b7e4b1492bb7f1c5ce0efdc269
Akash Chouhan <akash.c1500@gmail.com>
2021-10-26 00:11:57 +0530

    bug fix

--- BACKEND/config/DB.js
+++ BACKEND/config/DB.js
@@ -1,7 +1,7 @@
 // Mongo Database Connection: 
 
 const mongoose = require(`mongoose`);
-const MONGO_URI = process.env.MONGO_CLOUD_1 ||process.env.MONGO_URI || `mongodb://localhost:27017/temporary`
+const MONGO_URI = process.env.MONGO_CLOUD_URI || process.env.MONGO_LOCAL_URI
 
 let DBConnectionStatus = true;
 
