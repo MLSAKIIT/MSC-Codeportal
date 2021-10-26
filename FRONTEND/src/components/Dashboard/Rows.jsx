e25714abc3f618c838220000ce53875ba49e4bfb
Medhavi Basera <1905781@kiit.ac.in>
2021-10-26 23:16:55 +0530

    row color change on solved/unsolved

--- FRONTEND/src/components/Dashboard/Rows.jsx
+++ FRONTEND/src/components/Dashboard/Rows.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useEffect, useState } from "react";
 import "../Dashboard/QuestionList.css";
 import { Container, Row, Col, Table } from "react-bootstrap";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
@@ -9,10 +9,27 @@ import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
 export default function Rows(props) {
+  const [solved, setSolved] = useState(false);
+  const [favourite, setFavourite] = useState(false);
+
+  function handleChange1(e) {
+    if (solved == false) setSolved(true);
+    else setSolved(false);
+    console.log("clicked");
+  }
+  function handleChange2(e) {
+    if (favourite == false) setFavourite(true);
+    else setFavourite(false);
+
+    console.log("bookmarked");
+  }
+
+  console.log(solved);
+  // console.log(favourite);
   return (
-    <tr className="rows-bg">
+    <tr className={"rows-bg" + (solved === true ? " solved " : " unsolved ")}>
       <td>
-        <Checkbox {...label} />
+        <Checkbox {...label} onChange={handleChange1} />
       </td>
       <td>
         <Checkbox
@@ -20,10 +37,15 @@ export default function Rows(props) {
           icon={<BookmarkBorderIcon className="bookmark" />}
           checkedIcon={<BookmarkIcon className="bookmark" />}
           className="bookmark"
+          onChange={handleChange2}
         />
       </td>
-      <td>{props.no}</td>
-      <td>{props.problem}</td>
+      <td>
+        <div> {props.no}</div>
+      </td>
+      <a href={props.link} className="prob-style">
+        <td>{props.problem}</td>
+      </a>
     </tr>
   );
 }
