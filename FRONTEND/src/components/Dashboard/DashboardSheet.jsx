8b794efefe902827e9eb353f93c3581f6bac153d
Medhavi Basera <1905781@kiit.ac.in>
2021-10-30 00:08:47 +0530

    cards updated with new data

--- FRONTEND/src/components/Dashboard/DashboardSheet.jsx
+++ FRONTEND/src/components/Dashboard/DashboardSheet.jsx
@@ -1,14 +1,7 @@
 import React from "react";
-import {
-  Card,
-  CardGroup,
-  Image,
-  Row,
-  Col,
-  ProgressBar,
-  Button,
-} from "react-bootstrap";
+import { Card, Image, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import Cardsheet from "./Cardsheet";
 
 const DashboardSheet = () => {
   return (
@@ -16,26 +9,10 @@ const DashboardSheet = () => {
       style={{ marginTop: "50px", overflow: "hidden", paddingBottom: "20px" }}
     >
       <Row xs={1} md={3} style={{ marginLeft: "20px" }} className="g-4">
-        {Array.from({ length: 3 }).map((_, idx) => (
-          <Col>
-            <Link to="/dashboard-set" style={{textDecoration:"none",color:"#000"}}>
-              <Card className="w-75">
-                <Image
-                  src="/FRONTEND/src/components/About/Images/Learn.svg/50px20"
-                  roundedCircle
-                />
-                <Card.Body>
-                  <Card.Title>Sheet Name</Card.Title>
-                  <Card.Text className="px-2">
-                    This is a longer card with supporting text below as a
-                    natural lead-in to additional content. This content is a
-                    little bit longer.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Link>
-          </Col>
-        ))}
+        <Cardsheet title="Striver's Sheet"/>
+        <Cardsheet title="Dsa sheet by Fraz"/>
+        <Cardsheet title="450 Dsa sheet"/>
+
       </Row>
     </div>
   );
