fe8a00bce9b29b284d2cba9371e620d425181e38
Medhavi Basera <1905781@kiit.ac.in>
2021-11-13 13:34:36 +0530

    temp changes

--- FRONTEND/src/components/Dashboard/QuestionList.jsx
+++ FRONTEND/src/components/Dashboard/QuestionList.jsx
@@ -1,19 +1,222 @@
-import React, { useState, useEffect, useContext } from "react";
+// import React, { useState, useEffect } from "react";
+// import BootstrapTable from "react-bootstrap-table-next";
+// import ToolkitProvider from "react-bootstrap-table2-toolkit";
+// import Tooltip from "react-bootstrap/Tooltip";
+import Spinner from "react-bootstrap/Spinner";
+// import { toast, ToastContainer } from "react-toastify";
+// import "react-toastify/dist/ReactToastify.min.css";
+// import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+// import "./QuestionList.css";
+// import Fade from "react-reveal/Fade";
+
+// export default function QuestionList({ data, updateData }) {
+//   const [select, setSelected] = useState([]);
+//   const [questionsTableData, setQuestionsTableData] = useState([]);
+//   const [topicName, setTopicName] = useState("");
+
+//   useEffect(() => {
+//     let mounted = true;
+//     if (data !== undefined) {
+//       let doneQuestion = [];
+
+//       let tableData = data.questions.map((question, index) => {
+//         if (question.Done) {
+//           doneQuestion.push(index);
+//         }
+//         if (mounted) {
+//           return {
+//             id: index,
+//             question: (
+//               <>
+//                 {/* Question link */}
+//                 <a
+//                   href={question.URL}
+//                   target="_blank"
+//                   rel="noopener noreferrer"
+//                   style={{ fontWeight: "600" }}
+//                   className="question-link"
+//                 >
+//                   {question.Problem}
+//                 </a>
+//               </>
+//             ),
+
+//             _is_selected: question.Done,
+//             _search_text: question.Problem,
+//           };
+//         }
+//       });
+//       setQuestionsTableData(tableData);
+//       setTopicName(data.topicName);
+//       setSelected(doneQuestion);
+//       return () => (mounted = false);
+//     }
+//   }, [data]);
+//   console.log("table data : ", questionsTableData);
+
+//   // table config
+//   const columns = [
+//     {
+//       dataField: "id",
+//       text: "Q-Id",
+//       headerStyle: { width: "120px", fontSize: "15px" },
+//       headerAlign: "center",
+//     },
+//     {
+//       dataField: "question",
+//       text: "Questions",
+//       headerStyle: { fontSize: "15px" },
+//       headerAlign: "center",
+//     },
+//     {
+//       dataField: "_is_selected",
+//       text: "Is Selected",
+//       headerStyle: { fontSize: "15px" },
+//       hidden: true,
+//       sort: true,
+//     },
+//   ];
+//   const rowStyle = { fontSize: "13px" };
+//   const selectRow = {
+//     mode: "checkbox",
+//     style: { background: "#6ECB63" },
+//     selected: select,
+//     onSelect: handleSelect,
+//     hideSelectAll: true,
+//   };
+//   const sortMode = {
+//     dataField: "_is_selected",
+//     order: "asc",
+//   };
+
+//   // func() triggered when a question is marked done
+//   function handleSelect(row, isSelect) {
+//     let key = topicName.replace(/[^A-Z0-9]+/gi, "_").toLowerCase();
+//     let newDoneQuestion = [...select];
+//     let updatedQuestionsStatus = data.questions.map((question, index) => {
+//       if (row.id === index) {
+//         question.Done = isSelect;
+//         if (isSelect) {
+//           newDoneQuestion.push(row.id);
+//         } else {
+//           var pos = newDoneQuestion.indexOf(row.id);
+//           newDoneQuestion.splice(pos, 1);
+//         }
+//         return question;
+//       } else {
+//         return question;
+//       }
+//     });
+//     updateData(
+//       key,
+//       {
+//         started: newDoneQuestion.length > 0 ? true : false,
+//         doneQuestions: newDoneQuestion.length,
+//         questions: updatedQuestionsStatus,
+//       },
+//       data.position
+//     );
+//     displayToast(isSelect, row.id);
+//   }
+
+//   // trigger an information message for user on select change
+//   function displayToast(isSelect, id) {
+//     const { type, icon, dir } = {
+//       type: isSelect ? "Done" : "Incomplete",
+//       icon: isSelect ? "🎉" : "🙇🏻‍♂️",
+//       dir: isSelect ? "👇🏻" : "👆🏻",
+//     };
+
+//     const title = `${isSelect ? select.length + 1 : select.length - 1}/${
+//       data.questions.length
+//     } Done ${icon}`;
+//     const subTitle = `Question pushed ${dir} the table.`;
+
+//     const Card = (
+//       <>
+//         <p>{title}</p>
+//         <p className="toast-subtitle">{subTitle}</p>
+//       </>
+//     );
+
+//     toast(Card, {
+//       className: `toast-${type}`,
+//       autoClose: 2000,
+//       closeButton: true,
+//     });
+//   }
+
+//   return (
+//     <div className="question-landing">
+//       <h1 className="text-center mb-4 topic-name">{topicName}</h1>
+
+//       {data === undefined ? (
+//         <div className="d-flex justify-content-center">
+//           <Spinner animation="grow" variant="success" />
+//         </div>
+//       ) : (
+//         <ToolkitProvider
+//           className="float-right"
+//           keyField="id"
+//           data={questionsTableData}
+//           columns={columns}
+//           rowStyle={rowStyle}
+//         >
+//           {(props) => (
+//             <div>
+//               <div className="container " style={{ overflowAnchor: "none" }}>
+//                 <Fade duration={600}>
+//                   <BootstrapTable
+//                     {...props.baseProps}
+//                     selectRow={selectRow}
+//                     sort={sortMode}
+//                   />
+//                 </Fade>
+//               </div>
+//             </div>
+//           )}
+//         </ToolkitProvider>
+//       )}
+//       <ToastContainer />
+//     </div>
+//   );
+// }
+import React, { useState, useEffect } from "react";
 import BootstrapTable from "react-bootstrap-table-next";
 import ToolkitProvider from "react-bootstrap-table2-toolkit";
-import Spinner from "react-bootstrap/Spinner";
+import Fade from "react-reveal/Fade";
 import { Link } from "react-router-dom";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.min.css";
 import "react-bootstrap-table-next/dist/react-bootstrap-table2.min.css";
+
+// import "./Topic.css";
 import "./QuestionList.css";
 
-export default function QuestionList({ data, updateData }) {
+export default function Topic({ data, updateData }) {
+  console.log("REACHED HERE !!");
+
+  //  refreshPage();
+
+  // function refreshPage() {
+  //   window.location.reload(true);
+  // }
+  /*
+	  This component takes data releted to a paticular topic 
+	  and updateData() from App component
+	*/
+
+  /*
+	  Setting state for fields that comes from `data` prop 
+	  so that `data` prop is not undefined on reload
+	*/
   const [select, setSelected] = useState([]);
   const [questionsTableData, setQuestionsTableData] = useState([]);
   const [topicName, setTopicName] = useState("");
 
+  // updating states using useEffect with dependency  on `data` prop
   useEffect(() => {
+    // refreshPage();
     if (data !== undefined) {
       let doneQuestion = [];
 
@@ -21,7 +224,11 @@ export default function QuestionList({ data, updateData }) {
         if (question.Done) {
           doneQuestion.push(index);
         }
-
+        /*
+				|	Hidden properties `_is_selected` and `_search_text` are used to sort the table
+				|	and search the table respectively. react-bootstrap-table does not allow sorting
+				|	by selectRow by default, and requires plain text to perform searches.
+				*/
         return {
           id: index,
           question: (
@@ -46,35 +253,37 @@ export default function QuestionList({ data, updateData }) {
       setQuestionsTableData(tableData);
       setTopicName(data.topicName);
       setSelected(doneQuestion);
-    }
+    } 
+    // else {
+    //   console.log("Not defineddddddd");
+    // }
   }, [data]);
+  console.log("table data : ", questionsTableData);
 
   // table config
   const columns = [
     {
       dataField: "id",
       text: "Q-Id",
-      headerStyle: { width: "120px", fontSize: "15px" },
-      headerAlign: "center",
+      headerStyle: { width: "130px", fontSize: "20px" },
     },
     {
       dataField: "question",
       text: "Questions",
-      headerStyle: { fontSize: "15px" },
-      headerAlign: "center",
+      headerStyle: { fontSize: "20px" },
     },
     {
       dataField: "_is_selected",
       text: "Is Selected",
-      headerStyle: { fontSize: "15px" },
+      headerStyle: { fontSize: "20px" },
       hidden: true,
       sort: true,
     },
   ];
-  const rowStyle = { fontSize: "13px" };
+  const rowStyle = { fontSize: "20px" };
   const selectRow = {
     mode: "checkbox",
-    style: { background: "#6ECB63" },
+    style: { background: "#c8e6c9" },
     selected: select,
     onSelect: handleSelect,
     hideSelectAll: true,
@@ -142,8 +351,11 @@ export default function QuestionList({ data, updateData }) {
   }
 
   return (
-    <div className="question-landing">
-      <h1 className="text-center mb-4 topic-name">{topicName}</h1>
+    <>
+      <h3 className="text-center mb-4">
+        {/* <Link to="/">Topics</Link>/ */}
+        {topicName}
+      </h3>
 
       {data === undefined ? (
         <div className="d-flex justify-content-center">
@@ -156,22 +368,26 @@ export default function QuestionList({ data, updateData }) {
           data={questionsTableData}
           columns={columns}
           rowStyle={rowStyle}
-          search
         >
           {(props) => (
             <div>
-              <div className="container " style={{ overflowAnchor: "none" }}>
-                <BootstrapTable
-                  {...props.baseProps}
-                  selectRow={selectRow}
-                  sort={sortMode}
-                />
+              <div
+                className="container container-custom"
+                style={{ overflowAnchor: "none" }}
+              >
+                <Fade duration={600}>
+                  <BootstrapTable
+                    {...props.baseProps}
+                    selectRow={selectRow}
+                    sort={sortMode}
+                  />
+                </Fade>
               </div>
             </div>
           )}
         </ToolkitProvider>
       )}
       <ToastContainer />
-    </div>
+    </>
   );
 }
--- FRONTEND/src/services/dbServices2.js
+++ FRONTEND/src/services/dbServices2.js
@@ -1,38 +1,37 @@
 import Localbase from "localbase";
-import QuestionData, { version } from "../sheets/450DSAFinal-1";
+import QuestionData2, { version } from "../sheets/DSAsheetbyFraz";
 let db = new Localbase("db");
 window.db = db;
 db.config.debug = false;
-const localVersion = localStorage.getItem("450version");
+const localVersion = localStorage.getItem("");
 window.localVersion = localVersion;
 window.version = version;
 
-//insert function for sheet 1
-export function insertData(callback) {
-  QuestionData.forEach((topic, index) => {
-    db.collection("450dsaArchive").add(
+//insert function for sheet 2
+export function insertData2(callback) {
+  QuestionData2.forEach((topic, index) => {
+    db.collection("DSAsheetbyFraz").add(
       topic,
       topic.topicName.replace(/[^A-Z0-9]+/gi, "_").toLowerCase()
     );
   });
-  getData(callback);
+  getData2(callback);
 }
 
 //get data for sheet 1
-export function getData(callback) {
-  db.collection("450dsaArchive")
+export function getData2(callback) {
+  db.collection("DSAsheetbyFraz")
     .get()
     .then((data) => {
       if (data.length === 0) {
-        insertData(callback);
+        insertData2(callback);
       } else {
         data.sort((a, b) => {
           return a.position - b.position;
         });
         if (localVersion === null || localVersion === undefined) {
-          localStorage.setItem("450version", 100000000);
+          localStorage.setItem("DSAsheetbyFraz", 100000000);
           setTimeout(() => {
-            console.log("reload");
             window.location.reload();
           }, 3000);
         }
@@ -40,7 +39,7 @@ export function getData(callback) {
         if (parseInt(localVersion) !== version) {
           let i = 0;
           for (let topic of data) {
-            let dataFromJSON = QuestionData[i].questions;
+            let dataFromJSON = QuestionData2[i].questions;
             let len = dataFromJSON.length;
             let key = topic.topicName
               .replace(/[^A-Z0-9]+/gi, "_")
@@ -62,16 +61,15 @@ export function getData(callback) {
                 }
               }
             });
-            updateDBData(key, {
+            updateDBData2(key, {
               started: topic.started,
               doneQuestions: topic.doneQuestions,
               questions: dataFromJSON,
             });
             i++;
           }
-          localStorage.setItem("450version", version);
+          localStorage.setItem("DSAsheetbyFraz", version);
           setTimeout(() => {
-            console.log("reload");
             window.location.reload();
           }, 3000);
         } else {
@@ -82,8 +80,8 @@ export function getData(callback) {
 }
 
 //get topic data for sheet 1
-export function getTopicData(key, callback) {
-  db.collection("450dsaArchive")
+export function getTopicData2(key, callback) {
+  db.collection("DSAsheetbyFraz")
     .doc(key)
     .get()
     .then((document) => {
@@ -92,45 +90,45 @@ export function getTopicData(key, callback) {
 }
 
 //
-export function updateDBData(key, updateData) {
-  db.collection("450dsaArchive").doc(key).update(updateData);
+export function updateDBData2(key, updateData) {
+  db.collection("DSAsheetbyFraz").doc(key).update(updateData);
 }
 
-// export function resetDBData(callback) {
-//   db.collection("450dsaArchive")
-//     .delete()
-//     .then((response) => {
-//       callback(response);
-//     })
-//     .catch((error) => {
-//       console.log("There was an error, do something else", error);
-//     });
-// }
+export function resetDBData2(callback) {
+  db.collection("DSAsheetbyFraz")
+    .delete()
+    .then((response) => {
+      callback(response);
+    })
+    .catch((error) => {
+      console.log("There was an error, do something else", error);
+    });
+}
 
-// export function exportDBData(callback) {
-//   db.collection("450dsaArchive")
-//     .get()
-//     .then((data) => {
-//       callback(data);
-//     });
-// }
+export function exportDBData2(callback) {
+  db.collection("DSAsheetbyFraz")
+    .get()
+    .then((data) => {
+      callback(data);
+    });
+}
 
-// export function importDBData(data, callback) {
-//   resetDBData((response) => {
-//     new Promise((resolve, reject) => {
-//       data.forEach((topic, index) => {
-//         db.collection("450dsaArchive").add(
-//           topic,
-//           topic.topicName.replace(/[^A-Z0-9]+/gi, "_").toLowerCase()
-//         );
-//         if (index === data.length - 1) {
-//           resolve();
-//         }
-//       });
-//     }).then(() => {
-//       getData((data) => {
-//         callback(data);
-//       });
-//     });
-//   });
-// }
+export function importDBData2(data, callback) {
+  resetDBData2((response) => {
+    new Promise((resolve, reject) => {
+      data.forEach((topic, index) => {
+        db.collection("DSAsheetbyFraz").add(
+          topic,
+          topic.topicName.replace(/[^A-Z0-9]+/gi, "_").toLowerCase()
+        );
+        if (index === data.length - 1) {
+          resolve();
+        }
+      });
+    }).then(() => {
+      getData2((data) => {
+        callback(data);
+      });
+    });
+  });
+}
